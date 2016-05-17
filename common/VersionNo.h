90d95b357113a469ea6b578185fe1b2652ea711c
Yamagata Fumihiro <bun@kushiro-ct.ac.jp>
2016-05-17 23:42:59 +0900

    v3.40 Release version

--- common/VersionNo.h
+++ common/VersionNo.h
@@ -25,7 +25,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 /**/MAJOR_VERSION 3
 #define /*
 !define /**/ /*\
-/**/MINOR_VERSION 39
+/**/MINOR_VERSION 40
 #define /*
 !define /**/ /*\
 /**/EXTENDED_VERSION 2
